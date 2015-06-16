2f3ce392ae7c03de0872e4bd3776a0a07c73ff6a
aosiname <osinamea@gmail.com>
2015-06-16 11:48:24 +0100

    task2-cmd-prompt

---
+++ SalaryCheckerCMD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCheckerCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Name");
+            object empName = Console.ReadLine();
+            Console.WriteLine(empName.ToString());
+
+
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SalaryCheckerCMD/SalarySearcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalarySearcherTests;
+
+namespace SalaryCheckerCMD
+{
+    public class SalarySearcher : IEmployeeRepository, ISalaryRepository, ICurrencyRepository
+    {
+        public static object GetEmployeePayDetails()
+        {
+            return null;
+        }
+
+        public Currency GetCurrency(int currencyID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Salary GetEmployeeSalary(int employeeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
